8ba37d0a85e1c19c39569f3e3b17a6698f0a2cde
zapp <zapp_branegan@gmail.com>
2022-01-08 21:06:43 +0700

    AOC-2021 Day 16. Part2

--- day-16/main.rs
+++ day-16/main.rs
@@ -1,36 +1,44 @@
-use nom::combinator::{flat_map, map};
-use nom::multi::many_till;
-use nom::sequence::{preceded, tuple};
+use derive_try_from_primitive::TryFromPrimitive;
+use nom::combinator::map;
+use nom::multi::{length_count, many_till};
+use nom::sequence::preceded;
 use nom::{bits::complete::tag, bits::complete::take};
 use nom::{IResult, Parser};
+use std::hint::unreachable_unchecked;
 use std::{fs::read_to_string, u8};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Packet {
     version: u8,
     kind: PacketKind,
+    data: PacketData,
 }
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq, TryFromPrimitive)]
+#[repr(u8)]
 enum PacketKind {
-    Literal(u64),
-    Operator {
-        length_type: LengthType,
-        packets: Vec<Packet>,
-    },
+    Sum,
+    Prod,
+    Min,
+    Max,
+    Literal,
+    Gt,
+    Lt,
+    Eq,
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum LengthType {
-    Bits(u32),
-    Subpackets(u32),
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum PacketData {
+    Value(u64),
+    SubPackets(Vec<Packet>),
 }
+
 type ParseResult<'a, T> = IResult<(&'a [u8], usize), T>;
 
 fn remaining_len(input: (&[u8], usize)) -> usize {
     input.0.len() * 8 - input.1
 }
 
-fn parse_literal(input: (&[u8], usize)) -> ParseResult<PacketKind> {
+fn parse_literal(input: (&[u8], usize)) -> ParseResult<PacketData> {
     let group = preceded(tag(0b1, 1usize), take(4usize));
     let group_last = preceded(tag(0b0, 1usize), take(4usize));
     let groups = many_till(group, group_last);
@@ -44,70 +52,99 @@ fn parse_literal(input: (&[u8], usize)) -> ParseResult<PacketKind> {
                 let shifted_n = (n as u64) << ((i + 1) * 4);
                 acc + shifted_n
             });
-        PacketKind::Literal(literal)
+        PacketData::Value(literal)
     });
     parser.parse(input)
 }
-fn parse_operator(input: (&[u8], usize)) -> ParseResult<PacketKind> {
+fn parse_subpackets(input: (&[u8], usize)) -> ParseResult<PacketData> {
     let (input, length_type_id) = take(1usize)(input)?;
-    let mut bits = take(15usize).map(LengthType::Bits);
-    let mut packets = take(11usize).map(LengthType::Subpackets);
-    let (mut input, length_type) = match length_type_id {
-        0 => bits.parse(input)?,
-        1 => packets.parse(input)?,
-        _ => unreachable!(),
-    };
-    let mut packets = vec![];
-    match length_type {
-        LengthType::Bits(len) => {
-            let start_len = remaining_len(input);
-            let mut cur_len = start_len;
-            while start_len - cur_len < len as usize {
+    match length_type_id {
+        0 => {
+            let (mut input, len) = take(15usize)(input)?;
+            let mut packets = vec![];
+            let mut cur_len = remaining_len(input);
+            let start_len = cur_len;
+            while start_len - cur_len < len {
                 let (inner_input, packet) = Packet::parse_from_offset(input)?;
                 input = inner_input;
                 cur_len = remaining_len(input);
                 packets.push(packet);
             }
+            Ok((input, PacketData::SubPackets(packets)))
         }
-        LengthType::Subpackets(num) => {
-            while packets.len() < num as usize {
-                let (inner_input, packet) = Packet::parse_from_offset(input)?;
-                input = inner_input;
-                packets.push(packet);
-            }
+        1 => {
+            let num = take::<_, u16, _, _>(11usize);
+            let mut parser =
+                length_count(num, Packet::parse_from_offset).map(PacketData::SubPackets);
+            parser.parse(input)
         }
+        // SAFETY: match on 1 bit value
+        _ => unsafe { unreachable_unchecked() },
     }
-    Ok((
-        input,
-        PacketKind::Operator {
-            length_type,
-            packets,
-        },
-    ))
 }
 impl Packet {
     fn parse(input: &[u8]) -> ParseResult<Packet> {
         Self::parse_from_offset((input, 0))
     }
     fn parse_from_offset(input: (&[u8], usize)) -> ParseResult<Self> {
-        let version = take(3usize);
-        let type_id = take(3usize);
-        let kind = flat_map(type_id, |type_id| match type_id {
-            4 => parse_literal,
-            _ => parse_operator,
-        });
-        let mut parser = map(tuple((version, kind)), |(version, kind)| Packet {
-            version,
-            kind,
-        });
-        parser.parse(input)
+        let (input, version) = take(3usize)(input)?;
+        let (input, type_id) = take(3usize)(input)?;
+        let kind = PacketKind::try_from(type_id).expect("u8 -> PacketKind");
+        let (input, data) = match type_id {
+            4 => parse_literal(input)?,
+            _ => parse_subpackets(input)?,
+        };
+        Ok((
+            input,
+            Packet {
+                version,
+                kind,
+                data,
+            },
+        ))
     }
-    fn version_sum(&self) -> u32 {
-        match &self.kind {
-            PacketKind::Literal(_) => self.version as u32,
-            PacketKind::Operator { packets, .. } => {
-                packets.iter().map(|p| p.version_sum()).sum::<u32>() + self.version as u32
+    fn version_sum(&self) -> u64 {
+        match &self.data {
+            PacketData::Value(_) => self.version as u64,
+            PacketData::SubPackets(packets) => {
+                packets.iter().map(|p| p.version_sum()).sum::<u64>() + self.version as u64
             }
+        }
+    }
+    fn eval(&self) -> u64 {
+        match &self.kind {
+            PacketKind::Sum => match &self.data {
+                PacketData::Value(n) => *n,
+                PacketData::SubPackets(packets) => packets.iter().map(|p| p.eval()).sum(),
+            },
+            PacketKind::Prod => match &self.data {
+                PacketData::Value(n) => *n,
+                PacketData::SubPackets(packets) => packets.iter().map(|p| p.eval()).product(),
+            },
+            PacketKind::Min => match &self.data {
+                PacketData::Value(n) => *n,
+                PacketData::SubPackets(packets) => packets.iter().map(|p| p.eval()).min().unwrap(),
+            },
+            PacketKind::Max => match &self.data {
+                PacketData::Value(n) => *n,
+                PacketData::SubPackets(packets) => packets.iter().map(|p| p.eval()).max().unwrap(),
+            },
+            PacketKind::Literal => match self.data {
+                PacketData::Value(n) => n,
+                PacketData::SubPackets(_) => unreachable!("should be literal"),
+            },
+            PacketKind::Gt => match &self.data {
+                PacketData::Value(n) => *n,
+                PacketData::SubPackets(packets) => (packets[0].eval() > packets[1].eval()) as u64,
+            },
+            PacketKind::Lt => match &self.data {
+                PacketData::Value(n) => *n,
+                PacketData::SubPackets(packets) => (packets[0].eval() < packets[1].eval()) as u64,
+            },
+            PacketKind::Eq => match &self.data {
+                PacketData::Value(n) => *n,
+                PacketData::SubPackets(packets) => (packets[0].eval() == packets[1].eval()) as u64,
+            },
         }
     }
 }
@@ -126,63 +163,69 @@ mod tests {
     use super::*;
     use hex_literal::hex;
 
-    const PACKET: [u8; 3] = hex!("D2FE28");
-    const OPERATOR_PACKET1: [u8; 7] = hex!("38006F45291200");
-    const OPERATOR_PACKET2: [u8; 7] = hex!("EE00D40C823060");
     #[test]
     fn part1_1() -> anyhow::Result<()> {
+        const PACKET: [u8; 3] = hex!("D2FE28");
         let (_, packet) = Packet::parse(&PACKET)?;
         assert_eq!(
             packet,
             Packet {
                 version: 6,
-                kind: PacketKind::Literal(2021)
+                kind: PacketKind::Literal,
+                data: PacketData::Value(2021)
             }
         );
         Ok(())
     }
     #[test]
     fn part1_2() -> anyhow::Result<()> {
-        let (_, op_packet) = Packet::parse(&OPERATOR_PACKET1)?;
+        let (_, op_packet) = Packet::parse(&hex!("38006F45291200"))?;
         assert_eq!(
-            op_packet.kind,
-            PacketKind::Operator {
-                length_type: LengthType::Bits(27),
-                packets: vec![
+            op_packet,
+            Packet {
+                version: 1,
+                kind: PacketKind::Lt,
+                data: PacketData::SubPackets(vec![
                     Packet {
                         version: 6,
-                        kind: PacketKind::Literal(10)
+                        kind: PacketKind::Literal,
+                        data: PacketData::Value(10)
                     },
                     Packet {
                         version: 2,
-                        kind: PacketKind::Literal(20)
+                        kind: PacketKind::Literal,
+                        data: PacketData::Value(20)
                     },
-                ],
+                ],)
             }
         );
         Ok(())
     }
     #[test]
     fn part1_3() -> anyhow::Result<()> {
-        let (_, op_packet) = Packet::parse(&OPERATOR_PACKET2)?;
+        let (_, op_packet) = Packet::parse(&hex!("EE00D40C823060"))?;
         assert_eq!(
-            op_packet.kind,
-            PacketKind::Operator {
-                length_type: LengthType::Subpackets(3),
-                packets: vec![
+            op_packet,
+            Packet {
+                version: 7,
+                kind: PacketKind::Max,
+                data: PacketData::SubPackets(vec![
                     Packet {
                         version: 2,
-                        kind: PacketKind::Literal(1)
+                        kind: PacketKind::Literal,
+                        data: PacketData::Value(1),
                     },
                     Packet {
                         version: 4,
-                        kind: PacketKind::Literal(2)
+                        kind: PacketKind::Literal,
+                        data: PacketData::Value(2),
                     },
                     Packet {
                         version: 1,
-                        kind: PacketKind::Literal(3)
+                        kind: PacketKind::Literal,
+                        data: PacketData::Value(3),
                     },
-                ],
+                ],)
             }
         );
 
@@ -212,12 +255,53 @@ mod tests {
         assert_eq!(packet.version_sum(), 31);
         Ok(())
     }
-    //#[test]
-    //fn part2() -> anyhow::Result<()> {
-    //    let parsed = parse(INPUT)?;
-    //    let tiled_map = tiled(parsed, 5);
-    //    let tiled_map = RiskMap::new(tiled_map);
-    //    assert_eq!(path_risk_level(&tiled_map), 315);
-    //    Ok(())
-    //}
+
+    #[test]
+    fn part2_1() -> anyhow::Result<()> {
+        let (_, packet) = Packet::parse(&hex!("C200B40A82"))?;
+        assert_eq!(packet.eval(), 3);
+        Ok(())
+    }
+    #[test]
+    fn part2_2() -> anyhow::Result<()> {
+        let (_, packet) = Packet::parse(&hex!("04005AC33890"))?;
+        assert_eq!(packet.eval(), 54);
+        Ok(())
+    }
+    #[test]
+    fn part2_3() -> anyhow::Result<()> {
+        let (_, packet) = Packet::parse(&hex!("880086C3E88112"))?;
+        assert_eq!(packet.eval(), 7);
+        Ok(())
+    }
+    #[test]
+    fn part2_4() -> anyhow::Result<()> {
+        let (_, packet) = Packet::parse(&hex!("CE00C43D881120"))?;
+        assert_eq!(packet.eval(), 9);
+        Ok(())
+    }
+    #[test]
+    fn part2_5() -> anyhow::Result<()> {
+        let (_, packet) = Packet::parse(&hex!("D8005AC2A8F0"))?;
+        assert_eq!(packet.eval(), 1);
+        Ok(())
+    }
+    #[test]
+    fn part2_6() -> anyhow::Result<()> {
+        let (_, packet) = Packet::parse(&hex!("F600BC2D8F"))?;
+        assert_eq!(packet.eval(), 0);
+        Ok(())
+    }
+    #[test]
+    fn part2_7() -> anyhow::Result<()> {
+        let (_, packet) = Packet::parse(&hex!("9C005AC2F8F0"))?;
+        assert_eq!(packet.eval(), 0);
+        Ok(())
+    }
+    #[test]
+    fn part2_8() -> anyhow::Result<()> {
+        let (_, packet) = Packet::parse(&hex!("9C0141080250320F1802104A08"))?;
+        assert_eq!(packet.eval(), 1);
+        Ok(())
+    }
 }
