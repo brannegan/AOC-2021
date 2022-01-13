02f0cfb5550c55242c793dbab97fafa328da42fe
zapp <zapp.brannegan@gmail.com>
2022-01-13 12:40:57 +0700

    AOC-2021 DAY 17. Proper termination condition

--- day-17/main.rs
+++ day-17/main.rs
@@ -5,11 +5,7 @@ use nom::{
     sequence::{preceded, separated_pair},
     Finish, Parser,
 };
-use std::{
-    collections::HashSet,
-    fs::read_to_string,
-    ops::{Not, RangeInclusive},
-};
+use std::{collections::HashSet, fs::read_to_string, ops::RangeInclusive};
 
 #[derive(Debug, Clone)]
 struct TargetArea {
@@ -45,31 +41,29 @@ fn possible_velocities_count(ta: TargetArea) -> usize {
 }
 fn possible_velocities(ta: TargetArea) -> HashSet<(i32, i32)> {
     let mut vel_xs: Vec<RangeInclusive<i32>> = (1..)
-        .take_while(|n| *ta.x_range.start() > n * (n - 1) / 2)
-        .filter_map(|n| {
+        .map(|n| {
             let left = ta.x_range.start() + n * (n - 1) / 2;
             let right = ta.x_range.end() + n * (n - 1) / 2;
             let ceil = (left.rem_euclid(n) != 0) as i32;
             let start_vel_x_range = (left.div_euclid(n) + ceil)..=right.div_euclid(n);
-            start_vel_x_range
-                .is_empty()
-                .not()
-                .then(|| start_vel_x_range)
+            (n, start_vel_x_range)
         })
+        .take_while(|(n, _)| *ta.x_range.start() > n * (n - 1) / 2)
+        .map(|(_, start_vel_x_range)| start_vel_x_range)
         .collect();
     let vel_ys: Vec<RangeInclusive<i32>> = (1i32..)
-        //TODO: find proper terminating condition
-        .take_while(|n| (ta.y_range.end() - ta.y_range.start()) >= *n / 5)
-        .filter_map(|n| {
+        .map(|n| {
             let bottom = ta.y_range.start() + n * (n - 1) / 2;
             let top = ta.y_range.end() + n * (n - 1) / 2;
             let ceil = (bottom.rem_euclid(n) != 0) as i32;
             let start_vel_y_range = (bottom.div_euclid(n) + ceil)..=top.div_euclid(n);
-            start_vel_y_range
-                .is_empty()
-                .not()
-                .then(|| start_vel_y_range)
+            (n, start_vel_y_range)
         })
+        .take_while(|(n, start_vel_y_range)| {
+            let end_vel = start_vel_y_range.end() + (n - 1) * (-1);
+            end_vel >= *ta.y_range.start()
+        })
+        .map(|(_, start_vel_y_range)| start_vel_y_range)
         .collect();
     let last_x = vel_xs.last().unwrap().clone();
     vel_xs.resize(vel_ys.len(), last_x);
